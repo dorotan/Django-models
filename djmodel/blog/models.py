06d338c0a0f343369462718af3ff58faf288590d
Dorota Niezborała <d.niezborala@merixstudio.com>
2018-01-17 20:51:04 +0100

    Override the save method
    Add slug

--- djmodel/blog/models.py
+++ djmodel/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.encoding import smart_text
 from django.utils import timezone
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -17,11 +18,20 @@ class PostModel(models.Model):
 	nonactive     = models.NullBooleanField(default=True)
 	title         = models.CharField(max_length=30, verbose_name="Title", unique=True)
 	description   = models.TextField(null=True, blank=True)
+	slug          = models.SlugField(null=True, blank=True)
 	publish       = models.CharField(max_length=120, choices=PUBLISH_CHOICES , default="draft")
 	view_count    = models.IntegerField(default=0)
 	publish_date  = models.DateField(auto_now=False, auto_now_add=False, default=timezone.now)
 	author_email  = models.CharField(max_length=240, validators=[validate_author_email, validate_admin], null=True, blank=True)
 	second_author = models.EmailField(max_length=240, null=True, blank=True)
+
+	def save(self, *args, **kwargs):
+		if not self.slug:
+			if self.title:
+				self.slug = slugify(self.title)
+		# print("Hello there")
+		# self.title = "A new title" #overriding post title
+		super(PostModel, self).save(*args, **kwargs)
 
 	class Meta:
 		verbose_name = "Post"
