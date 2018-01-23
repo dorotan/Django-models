74961b1569536469ec751f84cf9754b486b6f500
Dorota Niezborała <d.niezborala@merixstudio.com>
2018-01-23 20:19:10 +0100

    Change the variables

--- djmodel/blog/models.py
+++ djmodel/blog/models.py
@@ -9,43 +9,83 @@ from django.utils.text import slugify
 from .validators import validate_author_email, validate_admin
 
 PUBLISH_CHOICES = (
-		("draft", "Draft"),
-		("publish", "Publish"),
-		("private", "Private"),
-	)
+	("draft", "Draft"),
+	("publish", "Publish"),
+	("private", "Private"),
+)
 
 
 class PublishedManager(models.Manager):
 	def get_queryset(self):
 		return super(PublishedManager,
-					 self).get_queryset()\
-						  .filter(status="published")
+			self).get_queryset()\
+			.filter(status="published")
 
 
 class PostModel(models.Model):
-	active        = models.BooleanField(default=True)
-	nonactive     = models.NullBooleanField(default=True)
-	title         = models.CharField(
-							max_length=30, 
-							verbose_name="Title", 
-							unique=True,
-							error_messages={
-								"unique": "This title is not unique. Please try again",
-								"blank": "This field has to be filled"
-								},
-							help_text="This field is important for the user",
-							)
-	description   = models.TextField(null=True, blank=True)
-	slug          = models.SlugField(max_length=250, null=True, blank=True, editable=False)
-	publish       = models.CharField(max_length=120, choices=PUBLISH_CHOICES , default="draft")
-	view_count    = models.IntegerField(default=0)
-	publish_date  = models.DateField(auto_now=False, auto_now_add=False, default=timezone.now)
-	author_email  = models.CharField(max_length=240, validators=[validate_author_email, validate_admin], null=True, blank=True)
-	second_author = models.EmailField(max_length=240, null=True, blank=True)
-	updated		  = models.DateTimeField(auto_now=True)
-	timestamp     = models.DateTimeField(auto_now_add=True)
-	objects       = models.Manager()
-	published     = PublishedManager()
+	active = models.BooleanField(
+		default=True
+		)
+	nonactive = models.NullBooleanField(
+		default=True
+		)
+	title = models.CharField(
+		max_length=30, 
+		verbose_name="Title", 
+		unique=True,
+		error_messages={
+			"unique": "This title is not unique. Please try again",
+			"blank": "This field has to be filled"
+		},
+		help_text="This field is important for the user",
+		)
+	description = models.TextField(
+		null=True, 
+		blank=True
+		)
+	slug = models.SlugField(
+		max_length=250, 
+		null=True, 
+		blank=True, 
+		editable=False
+		)
+	publish = models.CharField(
+		max_length=120, 
+		choices=PUBLISH_CHOICES, 
+		default="draft"
+		)
+	view_count = models.IntegerField(
+		default=0
+		)
+	publish_date = models.DateField(
+		auto_now=False, 
+		auto_now_add=False, 
+		default=timezone.now
+		)
+	author_email = models.CharField(
+		max_length=240, 
+		validators=[
+		validate_author_email, 
+		validate_admin]
+		, 
+		null=True, 
+		blank=True
+		)
+	second_author = models.EmailField(
+		max_length=240, 
+		null=True, 
+		blank=True
+		)
+	updated = models.DateTimeField(
+		auto_now=True
+		)
+	timestamp = models.DateTimeField(
+		auto_now_add=True
+		)
+	objects = models.Manager(
+		)
+	published = PublishedManager(
+		)
 
 	def save(self, *args, **kwargs):
 		if not self.slug:
